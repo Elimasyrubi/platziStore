8efe28a4db377ba86ffe572d0a31fb8528f1fa0d
Eliezer <elimasyrubi@gmail.com>
2022-01-09 12:10:20 -0300

    helms edited

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Products from '../components/Products';
-import initialState from '../initialState'
+import initialState from '../initialState';
+// import MetaHead from '../utils/helmet';
 
 const Home = () => {
+
   return (
-    <Products
-      products={initialState.products}
-    />
+    <>
+      {/* <MetaHead
+        title="Productos"
+        description="Encuentra todos tus productos favoritos"
+        image="https://davecast.s3.amazonaws.com/avatarnegativo.jpg"
+        url="https://mocafood.xyz/"
+      /> */}
+
+      <Products products={initialState.products} />
+    </>
   );
 }
 
